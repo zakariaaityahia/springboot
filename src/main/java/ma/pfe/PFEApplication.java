ab7d7e76207a202c4bee8d392b0db327f881ba08
zakariaeightyahia <zakariaeightyahia@gmail.com>
2023-02-12 19:41:16 +0100

    SpringBoot demo by zakaria ait yahia 05/02/2023

---
+++ src/main/java/ma/pfe/PFEApplication.java
@@ -0,0 +1,12 @@
+package ma.pfe;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PFEApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PFEApplication.class);
+    }
+}
